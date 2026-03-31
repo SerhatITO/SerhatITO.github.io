c181598073a99a4d75534fe1fc392f66d0622204
Serhat <116250007+SerhatITO@users.noreply.github.com>
2026-03-31 12:13:22 +0300

    Add files via upload

--- generate-pdf.js
+++ generate-pdf.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/resume.html', { waitUntil: 'networkidle0' });
     await page.pdf({
-        path: 'C:/Users/Serhat/Desktop/Serhat_Sahin_CV.pdf',
+        path: 'C:/Users/Serhat/Desktop/project/portfolio/Serhat_Sahin_CV.pdf',
         format: 'A4',
         margin: { top: '10mm', right: '10mm', bottom: '10mm', left: '10mm' },
         printBackground: true,
